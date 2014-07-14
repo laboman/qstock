11150465c536f299d00612ab99b6e9bb4f621307
laboman <janne@uleaborg.com>
2014-07-14 21:24:34 +0300

    start with clean

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,3 @@
-/*global console,jQuery,$,alert, qstock_get_bands, isStarred, device, qstock_data_init*/
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -20,42 +18,32 @@
  */
 var app = {
     // Application Constructor
-    initialize: function () {
-        "use strict";
+    initialize: function() {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        "use strict";
+    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        "use strict";
+    onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        "use strict";
-        var parentElement, listeningElement, receivedElement, devicePlatform;
-        parentElement = document.getElementById(id);
-
-        listeningElement = parentElement.querySelector('.listening');
-        receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-    
-        qstock_data_init();
-    },
-    callAnothePage : function (pagetocall) {
-        "use strict";
-        window.location = pagetocall;
+
+        console.log('Received Event: ' + id);
     }
 };
